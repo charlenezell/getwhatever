ee51fca064f002bcf12a0463012b958e481ec267
zellw <charlenezell@gmail.com>
2018-01-12 18:53:45 +0800

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({
+        width:750,
+        height:1144,
+        deviceScaleFactor:2,
+        isMobile:true,
+        hasTouch:true
+    })
+    await page.goto('http://m.100bt.com/zn/');
+    await page.screenshot({
+        path: 'example.png',
+        quality:100,
+        fullPage:true
+    });
+    await browser.close();
+})();
